b0611eeb311ef8098fcde8b106b1574e6a1be6fc
pierrekolins <pierrepneto@gmail.com>
2019-08-31 10:36:51 -0300

    Projeto criado

---
+++ InfoSistemas.dpr
@@ -0,0 +1,14 @@
+program InfoSistemas;
+
+uses
+  Forms,
+  UFrmPrincipal in 'UFrmPrincipal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
---
+++ UFrmPrincipal.pas
@@ -0,0 +1,24 @@
+unit UFrmPrincipal;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TFrmPrincipal = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmPrincipal: TFrmPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+end.
